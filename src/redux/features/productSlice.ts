21482efe489519c71ef782d3372741894ce06f59
Aswin <aswinss0018@gmail.com>
2025-01-06 07:34:16 +0530

    product schema is created

--- src/redux/features/productSlice.ts
+++ src/redux/features/productSlice.ts
@@ -1,20 +1,12 @@
+import { IProduct } from "@/app/admin/dashboard/page";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-
-interface IProduct {
-  _id: string;
-  imgSrc: string;
-  fileKey: string;
-  name: string;
-  price: number;
-  category: string;
-}
 
 const initialState: IProduct = {
   _id: "",
   imgSrc: "",
   fileKey: "",
   name: "",
-  price: 0,
+  price: "",
   category: "",
 };
 
