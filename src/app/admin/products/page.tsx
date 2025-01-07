32087b059ddf7f5ad13c2030e8b895ab4d778e58
Aswin <aswinss0018@gmail.com>
2025-01-07 15:08:35 +0530

    Table row is created

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,3 +1,11 @@
+import ProductForm from "@/components/admin-panel/ProductForm";
+
 export default function page() {
-  return <div>products</div>;
+  return (
+    <div className="h-[calc(100vh-96px)] w-full grid place-items-center overflow-y-auto">
+      <div className="bg-white w-[300px] rounded-lg p-4">
+        <ProductForm />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/admin-panel/ProductForm.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface IPayload {
+  name: string;
+  price: string;
+  category: string;
+  imgSrc: null | string;
+  fileKey: null | string;
+}
+
+export default function ProductForm() {
+  return <div>ProductForm</div>;
+}
