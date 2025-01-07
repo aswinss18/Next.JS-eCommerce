9ea2eb44173de56d09fd38329cfce220025a3b74
Aswin <aswinss0018@gmail.com>
2025-01-07 15:21:57 +0530

    Product form created.

--- src/components/admin-panel/ProductForm.tsx
+++ src/components/admin-panel/ProductForm.tsx
@@ -1,4 +1,10 @@
-import React from "react";
+"use client";
+
+import { setLoading } from "@/redux/features/loadingSlice";
+import { useAppDispatch } from "@/redux/hook";
+import { makeToast } from "@/utils/helper";
+import axios from "axios";
+import React, { FormEvent, useState } from "react";
 
 interface IPayload {
   name: string;
@@ -9,5 +15,36 @@ interface IPayload {
 }
 
 export default function ProductForm() {
+  const [payload, setPayload] = useState<IPayload>({
+    imgSrc: null,
+    name: "",
+    price: "",
+    category: "",
+    fileKey: null,
+  });
+
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    dispatch(setLoading(true));
+
+    axios
+      .post("/api/add_product", payload)
+      .then((res) => {
+        makeToast("Product added Successfully.");
+        setPayload({
+          imgSrc: null,
+          name: "",
+          price: "",
+          category: "",
+          fileKey: null,
+        });
+      })
+      .catch((err) => console.log(err))
+      .finally(() => dispatch(setLoading(false)));
+  };
+
   return <div>ProductForm</div>;
 }
