f22bcfd3ebd386c620ad8200051eac1de6b27f19
Aswin <aswinss0018@gmail.com>
2025-01-09 11:47:14 +0530

    delete file api created

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,6 +1,8 @@
 import { createRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
+import { NextRequest, NextResponse } from "next/server";
+import { UTApi } from "uploadthing/server";
 
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
@@ -9,3 +11,11 @@ export const { GET, POST } = createRouteHandler({
   // Apply an (optional) custom config:
   // config: { ... },
 });
+
+export async function DELETE(request: NextRequest, response: NextResponse) {
+  const { filekey } = await request.json();
+  const utApi = new UTApi();
+  await utApi.deleteFiles(filekey);
+
+  return NextResponse.json({ message: "file deleted" });
+}
